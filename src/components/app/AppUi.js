9489b97ca0238a61b8396cda85f00adc737272fd
Your Name <betancur10nicolas@gmail.com>
2023-05-09 14:18:58 -0500

    V1.0.0

--- src/components/app/AppUi.js
+++ src/components/app/AppUi.js
@@ -1,9 +1,7 @@
 import { NavBar } from "../NavBar";
 import { Carousel } from "../Carousel/index.js";
-import { useContext } from "react";
-import { Context } from "../context";
+
 export function AppUI() {
-    const { width } = useContext(Context)
     return (
         <div className="app">
             <NavBar />
